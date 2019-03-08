bf16e937cf5b005d9eeb8ffe009efcd96b5ab56e
dinara0708 <aikenova01@gmail.com>
2019-03-08 23:04:11 +0600

    Добавьте файлы проекта.

---
+++ LAYOUT_4/Source.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <iomanip>
+#include<math.h>
+#include<ctime>
+#include<cstring>
+
+using namespace std;
+
+void createArray(int *&a, int n) {
+	a = new int[n];
+}
+void fillArray(int *a, int n) {
+	for (int i = 0; i < n; i++)
+		a[i] = rand() % 50 - 10;
+}
+void printArray(const int *a, int n) {
+	for (int i = 0; i < n; i++)
+		cout << setw(3) << a[i] << " ";
+	cout << endl;
+}
+void deleteArray(int *&a) {
+	delete[] a;
+}
+
+
+int main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+
+	while (true)
+	{
+		cout << "Задача № ";
+		cin >> tn; cin.get();
+
+		switch (tn)
+		{
+		case 1:
+		{
+
+		}
+		break;
+		case 2:
+		{
+
+		}
+		break;
+		case 3:
+		{
+
+		}
+		break;
+		case 4:
+		{
+
+		}
+		break;
+		case 5:
+		{
+
+		}
+		break;
+		case 6:
+		{
+
+		}
+		break;
+		case 7:
+		{
+
+		}
+		break;
+		case 8:
+		{
+
+		}
+		break;
+		case 9:
+		{
+
+		}
+		break;
+		case 10:
+		{
+
+		}
+		break;
+		default:
+			cout << "нет такой задачи" << endl << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
