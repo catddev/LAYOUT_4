791ff94894abd0ed73c2c97061e4f70ae9e40196
dinara0708 <aikenova01@gmail.com>
2019-03-25 20:24:49 +0600

    -

--- LAYOUT_4/Source.cpp
+++ LAYOUT_4/Source.cpp
@@ -9,6 +9,27 @@
 
 using namespace std;
 
+template<typename T>
+void add(T*&cc, T c) {
+	if (buffer_size == 0)
+	{
+		buffer_size = 4;
+		cc = new T[buffer_size];
+	}
+	else
+	{
+		if (current_size == buffer_size)
+		{
+			buffer_size *= 2;
+			T*tmp = new T[buffer_size];
+			for (int i = 0; i < current_size; i++)
+				tmp[i] = cc[i];
+			delete[] cc;
+			cc = tmp;
+		}
+	}
+	cc[current_size++] = c;
+}
 void createArray(int *&a, int n) {
 	a = new int[n];
 }
